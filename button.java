40c0591ca8dcc960d05e214aaf4d0daf9a7c470a
Gautamhirawat <113746508+Gautamhirawat@users.noreply.github.com>
2024-01-02 15:36:02 +0530

    Update button.java

--- button.java
+++ button.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 
-
 public class button extends JFrame{ 
-
 
 /*we have to give name of our file TODO (YOU CAN GIVE ANY NAME) 
 then we have extended the JFrame it helps us to use all the function inside it.
@@ -11,21 +9,22 @@ then we have extended the JFrame it helps us to use all the function inside it.
     
     public button(){
 
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 400);
         setLocationRelativeTo(null);
         setTitle("My Swing App");
+
 /* Here i have added some buttons to the screen and we can change location of thoese buttons by 
  setBounds fnx .  */
+ 
         JButton button1 = new JButton("NEW");
-        button1.setBounds(50, 50, 100, 30);
+        button1.setBounds(10, 330, 100, 30);
 
         JButton button2 = new JButton("DELETE");
-        button2.setBounds(200, 50, 100, 30);
+        button2.setBounds(160, 330, 100, 30);
 
         JButton button3 = new JButton("SAVE");
-        button3.setBounds(350, 50, 100, 30);
+        button3.setBounds(310, 330, 100, 30);
 
         getContentPane().setLayout(null);
 
